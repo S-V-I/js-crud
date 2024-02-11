48024a1cb589677fa22a6ad266ba41087e890fa8
S-V-I <ambitious.sv@gmail.com>
2024-02-11 18:16:28 +0200

    crud-2

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,58 @@ const router = express.Router()
 
 // ================================================================
 
+class Product {
+  static #list = []
+
+  constructor(name, price, description) {
+    this.id = Math.floor(Math.random() * 100000)
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+
+  static getList = () => this.#list
+
+  static add(product) {
+    if (product instanceof Product) {
+      this.#list.push(product)
+    }
+  }
+
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+
+  static updateById(id, data) {
+    const product = this.getById(id)
+    if (product) {
+      this.update(use, data)
+      return true
+    } else {
+      return false
+
+      // product.name = data.name || product.name
+      // product.price = data.price || product.price
+      // product.description =
+      //   data.description || product.description
+    }
+  }
+
+  static deleteById(id) {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
